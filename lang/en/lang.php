30ac420c566d6d99b946ed29f1ace165c097c698
Valera Malyshev <billomania@ya.ru>
2021-05-05 06:22:23 +0300

    English language added

---
+++ lang/en/lang.php
@@ -0,0 +1,56 @@
+<?php return [
+    'plugin'      => [
+        'name'          		=> 'Additional fields for Shopaholic',
+        'description'   		=> 'Free plugin extending the functionality of the Shopaholic component',
+    ],
+    'menu'      => [
+        'label'         		=> 'Fields',
+    ],
+    'form'      => [
+        'list_title'    		=> 'Additional fields',
+        'create'        		=> 'Create',
+        'create_and_close'  	=> 'Create and close',
+        'saving'		    	=> 'The field is saved',
+        'cancel'		    	=> 'Cancel',
+        'delete_sure'		    => 'Are you sure you want to delete this field?',
+        'deleting'		   		=> 'Removed',
+        'save'		   			=> 'Save',
+        'save_and_close'		=> 'Save and close',
+    ],
+    'btn'      => [
+        'new_field'         	=> 'New field',
+        'delete_selected'   	=> 'Delete selected',
+        'delete_selected_sure'  => 'Are you sure you want to delete these fields?',
+        'saving'  				=> 'Persists ...',
+        'save'  				=> 'Save',
+        'save_and_close'  		=> 'Save and Close',
+    ],
+    'field'      => [
+        'name'         			=> 'Field name',
+        'slug'         			=> 'Slug',
+        'type'         			=> 'Field type',
+        'module'         		=> 'Module',
+        'options'         		=> 'Options',
+        'active'                => 'Active field',
+    ],
+    'type'       => [
+        'text'         			=> 'Text',
+        'textarea'         		=> 'Textarea',
+        'dropdown'         		=> 'Dropdown',
+        'checkbox'         		=> 'Checkbox',
+        'radio'         		=> 'Radio',
+        'number'                => 'Number',
+        'email'                 => 'E-mail',
+    ],
+    'module'       => [
+        'product'         		=> 'Product',
+        'offer'         		=> 'Offer',
+        'category'         		=> 'Category',
+        'brand'         		=> 'Brand',
+    ],
+    'active'       => [
+        'on'                    => 'Yea',
+        'off'                   => 'No',
+        'comment'               => 'If disabled, it is not displayed in forms',
+    ],
+];
